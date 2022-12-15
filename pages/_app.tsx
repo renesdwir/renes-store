fcb39c8b68a985ef34f252e8652cae4529b459f2
Renes Dwi Riwanto <renesriwanto@gmail.com>
2022-12-15 21:09:27 +0700

    sign-in

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import "../styles/homepage.css";
 import "../styles/utilities.css";
 import "../styles/checkout.css";
 import "../styles/complete-checkout.css";
+import "../styles/sign-in.css";
 import "../styles/detail.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";

88ec822a518d544d96ca7fb780f402349cad1492
Renes Dwi Riwanto <renesriwanto@gmail.com>
2022-12-17 11:49:40 +0700

    refactor is login & grouping className

--- components/sections/Navbar/index.tsx
+++ components/sections/Navbar/index.tsx
@@ -1,25 +1,18 @@
 import Image from "next/image";
+import Auth from "./Auth";
 import Menu from "./Menu";
+import ToggleMenu from "./ToggleMenu";
+import { NavClassName } from "../../../styles/grouping-classname";
 
 export default function Navbar() {
   return (
     <section>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light bg-white pt-lg-40 pb-lg-40 pt-30 pb-50">
+      <nav className={NavClassName}>
         <div className="container-fluid">
           <a className="navbar-brand" href="#">
             <Image src="/icon/logo.svg" alt="" width={60} height={60} />
           </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
+          <ToggleMenu />
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav ms-auto text-lg gap-lg-0 gap-2">
               <Menu title={"Home"} href={"/"} active />
@@ -27,15 +20,7 @@ export default function Navbar() {
               <Menu title={"Rewards"} href={"/"} />
               <Menu title={"Discover"} href={"/"} />
               <Menu title={"Global Rank"} href={"/"} />
-              <li className="nav-item my-auto">
-                <a
-                  className="btn btn-sign-in d-flex justify-content-center ms-lg-2 rounded-pill"
-                  href="./src/sign-in.html"
-                  role="button"
-                >
-                  Sign In
-                </a>
-              </li>
+              <Auth isLogin />
             </ul>
           </div>
         </div>
---
+++ styles/grouping-classname/index.tsx
@@ -0,0 +1,2 @@
+export const NavClassName =
+  "navbar navbar-expand-lg navbar-light bg-light bg-white pt-lg-40 pb-lg-40 pt-30 pb-50";

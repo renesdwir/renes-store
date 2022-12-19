6b4544d9e193fed623ef32d04287d085f2e40f60
Renes Dwi Riwanto <renesriwanto@gmail.com>
2022-12-19 19:50:13 +0700

    change tag a to Link

--- pages/complete-checkout.tsx
+++ pages/complete-checkout.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function CompleteCheckout() {
   return (
     <section className="complete-checkout mx-auto pt-lg-145 pb-lg-145 pt-100 pb-80">
@@ -263,13 +265,13 @@ export default function CompleteCheckout() {
           </p>
         </div>
         <div className="button-group d-flex flex-column mx-auto">
-          <a
+          <Link
             className="btn btn-dashboard fw-medium text-lg text-white rounded-pill mb-16"
-            href="../member/overview.html"
+            href="/"
             role="button"
           >
             My Dashboard
-          </a>
+          </Link>
           <a
             className="btn btn-whatsapp fw-medium text-lg color-palette-1 rounded-pill"
             href="#"

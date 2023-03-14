32b852645ea055514ca4c768a5dddd91dc158897
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-03-14 23:23:19 +0700

    refactor getfeaturedgame api

--- services/player.ts
+++ services/player.ts
@@ -2,6 +2,17 @@ import axios from "axios";
 export const getFeaturedGame = async () => {
   try {
     const BASE_URL = process.env.NEXT_PUBLIC_API;
+    const API_VERSION = "api/v1";
+    const URL = "players/landingpage";
+    const response = await axios.get(`${BASE_URL}/${API_VERSION}/${URL}`);
+    return response.data.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+export const getDetailVoucher = async () => {
+  try {
+    const BASE_URL = process.env.NEXT_PUBLIC_API;
     const response = await axios.get(`${BASE_URL}/api/v1/players/landingpage`);
   } catch (error) {
     console.log(error);

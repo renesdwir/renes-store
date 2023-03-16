e50909ff534dbb01dd1f1c2c1202251f1fc236c2
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-03-16 19:21:04 +0700

    add props id

--- components/items/GameItem/index.tsx
+++ components/items/GameItem/index.tsx
@@ -5,13 +5,14 @@ export interface GameItemProps {
   title: string;
   category: string;
   thumbnail: string;
+  id: string;
 }
 
 export default function GameItem(props: GameItemProps) {
-  const { title, category, thumbnail } = props;
+  const { title, category, thumbnail, id } = props;
   return (
     <div className="featured-game-card position-relative">
-      <Link href="/detail">
+      <Link href={`/detail/${id}`}>
         <div className="blur-sharp">
           <Image
             className="thumbnail"

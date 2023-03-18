4b4912d642cfdbd41f472b2e771eebf3fb2a1600
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-03-18 23:07:36 +0700

    set data to topupitem comp

--- components/sections/TopUpItem/index.tsx
+++ components/sections/TopUpItem/index.tsx
@@ -1,9 +1,17 @@
 interface TopUpItemProps {
   type: "desktop" | "mobile";
+  data: {
+    name: string;
+    thumbnail: string;
+    category: {
+      name: string;
+    };
+  };
 }
 
 export default function TopUpItem(props: TopUpItemProps) {
   const { type, data } = props;
+  const ROOT_IMG = process.env.NEXT_PUBLIC_IMG;
   if (type === "desktop") {
     return (
       <div className="pb-50 d-md-block d-none">
@@ -20,7 +28,7 @@ export default function TopUpItem(props: TopUpItemProps) {
     <div className="row align-items-center">
       <div className="col-md-12 col-4">
         <img
-          src="/img/Thumbnail-3.png"
+          src={`${ROOT_IMG}/${data.thumbnail}`}
           width="280"
           height="380"
           className="img-fluid"
@@ -30,12 +38,10 @@ export default function TopUpItem(props: TopUpItemProps) {
 
       <div className="col-md-12 col-8 d-md-none d-block">
         <h2 className="text-xl fw-bold color-palette-1 text-start mb-10">
-          Mobile Legends:
-          <br />
-          The New Battle 2021
+          {data.name}
         </h2>
         <p className="text-sm color-palette-2 text-start mb-0">
-          Category: Mobile
+          Category: {data.category.name}
         </p>
       </div>
     </div>

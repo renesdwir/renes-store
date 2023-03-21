9307d2deabb54ba233619facb5cdbbd8f73bb4bf
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-03-21 21:59:20 +0700

    add props payment item

--- components/sections/TopUpForm/index.tsx
+++ components/sections/TopUpForm/index.tsx
@@ -43,42 +43,10 @@ export default function TopUpForm() {
         </p>
         <fieldset id="paymentMethod">
           <div className="row justify-content-between">
-            <PaymentItem />
-            <label
-              className="col-lg-4 col-sm-6 ps-md-15 pe-md-15 pt-md-15 pb-md-15 pt-10 pb-10"
-              htmlFor="visa"
-            >
-              <input
-                className="d-none"
-                type="radio"
-                id="visa"
-                name="paymentMethod"
-                value="visa"
-              />
-              <div className="detail-card">
-                <div className="d-flex justify-content-between">
-                  <p className="text-3xl color-palette-1 fw-medium m-0">VISA</p>
-                  <svg
-                    id="icon-check"
-                    width="20"
-                    height="20"
-                    viewBox="0 0 20 20"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <circle cx="10" cy="10" r="10" fill="#CDF1FF" />
-                    <path
-                      d="M5.83301 10L8.46459 12.5L14.1663 7.5"
-                      stroke="#00BAFF"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    />
-                  </svg>
-                </div>
-                <p className="text-lg color-palette-1 m-0">Credit Card</p>
-              </div>
-            </label>
+            <PaymentItem bankId="123" type="Transfer" name="BCA" />
+            <PaymentItem bankId="1223" type="Transfer" name="Mandiri" />
+            <PaymentItem bankId="1323" type="Transfer" name="Gopay" />
+
             <div className="col-lg-4 col-sm-6"></div>
           </div>
         </fieldset>

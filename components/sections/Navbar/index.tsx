9c0cf6edd379f2cd9fb1d9b0e966ab829faf4f0b
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-03-26 20:07:44 +0700

    setup signup form

--- components/sections/Navbar/index.tsx
+++ components/sections/Navbar/index.tsx
@@ -20,7 +20,7 @@ export default function Navbar() {
               <Menu title={"Rewards"} href={"/"} />
               <Menu title={"Discover"} href={"/"} />
               <Menu title={"Global Rank"} href={"/"} />
-              <Auth isLogin />
+              <Auth />
             </ul>
           </div>
         </div>

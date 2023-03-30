b6f99eac2cd297ba08f65e1fc433b37e79b1043b
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-03-30 20:44:59 +0700

    setsignup API

---
+++ services/auth.ts
@@ -0,0 +1,15 @@
+import axios from "axios";
+const BASE_URL = process.env.NEXT_PUBLIC_API;
+const API_VERSION = "api/v1";
+export const setSignUp = async (data) => {
+  try {
+    const URL = "auth/signup";
+    const response = await axios.post(
+      `${BASE_URL}/${API_VERSION}/${URL}`,
+      data
+    );
+    return response.data.data;
+  } catch (error) {
+    console.log(error);
+  }
+};

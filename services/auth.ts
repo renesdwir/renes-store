5e8c58ea4f4b95388f5c773c14d967294cfd5d75
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-04-02 06:03:42 +0700

    set signin api

--- services/auth.ts
+++ services/auth.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { LoginTypes } from "./dataTypes";
 const BASE_URL = process.env.NEXT_PUBLIC_API;
 const API_VERSION = "api/v1";
 export const setSignUp = async (data) => {
@@ -7,11 +8,36 @@ export const setSignUp = async (data) => {
     const response = await axios
       .post(`${BASE_URL}/${API_VERSION}/${URL}`, data)
       .catch((err) => err.response);
-    if (response.error === 1) {
-      return response.data;
-    }
+    // if (response.error === 1) {
+    //   return response.data;
+    // }
     return response.data.data;
   } catch (error) {
     console.log(error);
   }
 };
+
+export async function setLogin(data: LoginTypes) {
+  try {
+    const URL = "auth/signin";
+    const response = await axios
+      .post(`${BASE_URL}/${API_VERSION}/${URL}`, data)
+      .catch((err) => err.response);
+    // if (response.status  > 300) {
+    // const res = {
+    //   error:true,
+    //   message:response.data.message
+    //   data: null
+    // }
+    //   return res;
+    // }
+    const res = {
+      error: false,
+      message: "success",
+      data: response.data.data,
+    };
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+}

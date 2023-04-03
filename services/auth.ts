1a40ff194a1f12f3a621f7bd7a810577a2f91b7f
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-04-03 21:16:12 +0700

    refactor api

--- services/auth.ts
+++ services/auth.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { LoginTypes } from "./dataTypes";
+import callAPI from "../config/api";
 const BASE_URL = process.env.NEXT_PUBLIC_API;
 const API_VERSION = "api/v1";
 export const setSignUp = async (data) => {
@@ -19,24 +20,8 @@ export const setSignUp = async (data) => {
 
 export async function setLogin(data: LoginTypes) {
   try {
-    const URL = "auth/signin";
-    const response = await axios
-      .post(`${BASE_URL}/${API_VERSION}/${URL}`, data)
-      .catch((err) => err.response);
-    // if (response.status  > 300) {
-    // const res = {
-    //   error:true,
-    //   message:response.data.message,
-    //   data: null
-    // }
-    //   return res;
-    // }
-    const res = {
-      error: false,
-      message: "success",
-      data: response.data.data,
-    };
-    return res;
+    const url = `${BASE_URL}/${API_VERSION}/auth/signin`;
+    return callAPI({ url, method: "POST", data });
   } catch (error) {
     console.log(error);
   }

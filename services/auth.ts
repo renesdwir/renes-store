c8170005f7de8cca50906c8156679c49ff7fc85c
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-04-04 21:09:07 +0700

    refactor signup api

--- services/auth.ts
+++ services/auth.ts
@@ -5,14 +5,16 @@ const BASE_URL = process.env.NEXT_PUBLIC_API;
 const API_VERSION = "api/v1";
 export const setSignUp = async (data) => {
   try {
-    const URL = "auth/signup";
-    const response = await axios
-      .post(`${BASE_URL}/${API_VERSION}/${URL}`, data)
-      .catch((err) => err.response);
-    // if (response.error === 1) {
-    //   return response.data;
-    // }
-    return response.data.data;
+    const url = `${BASE_URL}/${API_VERSION}/auth/signup`;
+    // const URL = "auth/signup";
+    // const response = await axios
+    //   .post(`${BASE_URL}/${API_VERSION}/${URL}`, data)
+    //   .catch((err) => err.response);
+    // // if (response.error === 1) {
+    // //   return response.data;
+    // // }
+    // return response.data.data;
+    return callAPI({ url, method: "POST", data });
   } catch (error) {
     console.log(error);
   }

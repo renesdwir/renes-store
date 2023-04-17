a3302ff60938b11e83c7597fa3f4af80a75702f6
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-04-17 21:42:14 +0700

    get serversideprops in member

--- pages/member/index.tsx
+++ pages/member/index.tsx
@@ -1,5 +1,7 @@
+import jwtDecode from "jwt-decode";
 import OverviewContent from "../../components/sections/OverviewContent";
 import SideBar from "../../components/sections/SideBar";
+import { JWTPayloadTypes, UserTypes } from "../../services/dataTypes";
 
 export default function Member() {
   return (
@@ -8,4 +10,32 @@ export default function Member() {
       <OverviewContent />
     </section>
   );
+}
+interface GetServerSideProps {
+  req: {
+    cookies: {
+      token: string;
+    };
+  };
+}
+export async function getServerSideProps({ req }: GetServerSideProps) {
+  const { token } = req.cookies;
+  if (!token) {
+    return {
+      redirect: {
+        destination: "/sign-in",
+        permanent: false,
+      },
+    };
+  }
+  const jwtToken = Buffer.from(token, "base64").toString("ascii");
+  const payload: JWTPayloadTypes = jwtDecode(jwtToken);
+  const userPayload: UserTypes = payload.player;
+  const IMG = process.env.NEXT_PUBLIC_IMG;
+  userPayload.avatar = `${IMG}/${userPayload.avatar}`;
+  return {
+    props: {
+      user: userPayload,
+    },
+  };
 }

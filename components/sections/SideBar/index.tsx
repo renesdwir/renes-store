271bed83a3f6c293859d8b4b5ebb75de9def17da
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-04-21 22:11:22 +0700

    onlogout function

--- components/sections/SideBar/index.tsx
+++ components/sections/SideBar/index.tsx
@@ -1,12 +1,19 @@
+import Cookies from "js-cookie";
 import Footer from "./Footer";
 import MenuItem from "./MenuItem";
 import Profile from "./Profile";
+import { useRouter } from "next/router";
 
 interface SideBarProps {
   activeMenu: "overview" | "transactions" | "settings";
 }
 export default function SideBar(props: SideBarProps) {
+  const router = useRouter();
   const { activeMenu } = props;
+  const onLogOut = () => {
+    Cookies.remove("token");
+    router.push("/sign-in");
+  };
   return (
     <section className="sidebar">
       <div className="content pt-50 pb-30 ps-30">
@@ -33,7 +40,7 @@ export default function SideBar(props: SideBarProps) {
             href="/member/edit-profile"
             active={activeMenu === "settings"}
           />
-          <MenuItem title="Log Out" icon="logout" href="/sign-in" />
+          <MenuItem title="Log Out" icon="logout" onClick={onLogOut} />
         </div>
         <Footer />
       </div>

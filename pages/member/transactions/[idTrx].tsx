83eea40aec9834c47146828c4425acb3e8369b52
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-04-26 21:18:24 +0700

    add private route

--- pages/member/transactions/[idTrx].tsx
+++ pages/member/transactions/[idTrx].tsx
@@ -1,5 +1,7 @@
+import jwtDecode from "jwt-decode";
 import SideBar from "../../../components/sections/SideBar";
 import TransactionDetailContent from "../../../components/sections/TransactionDetailContent";
+import { JWTPayloadTypes, UserTypes } from "../../../services/dataTypes";
 
 export default function TransactionsDetail() {
   return (
@@ -8,4 +10,33 @@ export default function TransactionsDetail() {
       <TransactionDetailContent />
     </section>
   );
+}
+interface GetServerSideProps {
+  req: {
+    cookies: {
+      token: string;
+    };
+  };
+}
+export async function getServerSideProps({ req }: GetServerSideProps) {
+  const { token } = req.cookies;
+  if (!token) {
+    return {
+      redirect: {
+        destination: "/sign-in",
+        permanent: false,
+      },
+    };
+  }
+
+  const jwtToken = Buffer.from(token, "base64").toString("ascii");
+  const payload: JWTPayloadTypes = jwtDecode(jwtToken);
+  const userPayload: UserTypes = payload.player;
+  const IMG = process.env.NEXT_PUBLIC_IMG;
+  userPayload.avatar = `${IMG}/${userPayload.avatar}`;
+  return {
+    props: {
+      user: userPayload,
+    },
+  };
 }

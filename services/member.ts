b25cfa2bb85b4106affdddc5cb68ff47ee65ea99
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-04-29 18:46:13 +0700

    update profile api

--- services/member.ts
+++ services/member.ts
@@ -27,3 +27,8 @@ export async function getTransactionDetail(id: string, token: string) {
   const url = `${BASE_URL}/${API_VERSION}/players/history/${id}/detail`;
   return callAPI({ url, method: "GET", serverToken: token });
 }
+
+export async function updateProfile(data: FormData, id: string) {
+  const url = `${BASE_URL}/${API_VERSION}/players/profile/${id}`;
+  return callAPI({ url, method: "PUT", data, token: true });
+}

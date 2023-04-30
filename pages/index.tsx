056c9bef7ecfc1f379609d94db410a7fbe503eb4
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-04-30 18:45:25 +0700

    set up basic seo

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ import FeaturedGame from "../components/sections/FeaturedGame";
 import Reached from "../components/sections/Reached";
 import Story from "../components/sections/Story";
 import Footer from "../components/sections/Footer";
+import Head from "next/head";
 
 export default function Home() {
   useEffect(() => {
@@ -14,6 +15,23 @@ export default function Home() {
   }, []);
   return (
     <>
+      <Head>
+        <title>RenesStore - Get a New Experience in Gaming</title>
+        <meta
+          name="description"
+          content="Kami menyediakan jutaan cara untuk membantu player menjadi pemenang sejati"
+        />
+        <meta
+          property="og:title"
+          content="RenesStore - Get a New Experience in Gaming"
+        />
+        <meta
+          property="og:description"
+          content="Kami menyediakan jutaan cara untuk membantu player menjadi pemenang sejati"
+        />
+        <meta property="og:image" content="" />
+        <meta property="og:url" content="https://renesStore.com" />
+      </Head>
       <Navbar />
       <MainBanner />
       <TransactionStep />

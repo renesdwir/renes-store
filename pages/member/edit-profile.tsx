2d8a50f1c5ae893daf633e3cee63e181fd23a5db
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-05-01 20:17:18 +0700

    edit profile

--- pages/member/edit-profile.tsx
+++ pages/member/edit-profile.tsx
@@ -1,8 +1,46 @@
 import Image from "next/image";
 import Input from "../../components/basics/Input";
 import SideBar from "../../components/sections/SideBar";
+import { useEffect, useState } from "react";
+import Cookies from "js-cookie";
+import { JWTPayloadTypes, UserTypes } from "../../services/dataTypes";
+import jwtDecode from "jwt-decode";
+import { updateProfile } from "../../services/member";
+import { toast } from "react-toastify";
+import { useRouter } from "next/router";
 
 export default function EditProfile() {
+  const router = useRouter();
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    avatar: "",
+    id: "",
+  });
+  const [imagePreview, setImagePreview] = useState(null);
+  useEffect(() => {
+    const token = Cookies.get("token");
+    if (token) {
+      const jwtToken = atob(token);
+      const payload: JWTPayloadTypes = jwtDecode(jwtToken);
+      const userFromPayload: UserTypes = payload.player;
+      const IMG = process.env.NEXT_PUBLIC_IMG;
+      userFromPayload["avatar"] = `${IMG}/${userFromPayload.avatar}`;
+      setUser(userFromPayload);
+    }
+  }, []);
+  const onSubmit = async () => {
+    const data = new FormData();
+    data.append("image", user.avatar);
+    data.append("name", user.name);
+    const response = await updateProfile(data, user.id);
+    if (response.error) {
+      toast.error(response.message);
+    } else {
+      Cookies.remove("token");
+      router.push("/sign-in");
+    }
+  };
   return (
     <section className="edit-profile overflow-auto">
       <SideBar activeMenu="settings" />
@@ -12,54 +50,59 @@ export default function EditProfile() {
           <div className="bg-card pt-30 ps-30 pe-30 pb-30">
             <form action="">
               <div className="photo d-flex">
-                <div className="position-relative me-20">
-                  <Image
-                    src="/img/avatar-1.png"
-                    width={90}
-                    height={90}
-                    className="avatar img-fluid"
-                    alt="Logo-avatar"
-                  />
-                  <div className="avatar-overlay position-absolute top-0 d-flex justify-content-center align-items-center">
-                    <Image
-                      src="/icon/overlay.svg"
-                      width={24}
-                      height={24}
-                      alt="overlay"
-                    />
-                  </div>
-                </div>
                 <div className="image-upload">
                   <label htmlFor="avatar">
-                    <Image
-                      src="/icon/upload.svg"
-                      width={90}
-                      height={90}
-                      alt="upload-logo"
-                    />
+                    {imagePreview ? (
+                      <Image
+                        src={imagePreview}
+                        width={90}
+                        height={90}
+                        style={{ borderRadius: "100%" }}
+                        alt="upload-logo"
+                      />
+                    ) : (
+                      <Image
+                        src={user.avatar}
+                        width={90}
+                        height={90}
+                        alt="upload-logo"
+                        style={{ borderRadius: "100%" }}
+                      />
+                    )}
                   </label>
                   <input
                     id="avatar"
                     type="file"
                     name="avatar"
                     accept="image/png, image/jpeg"
+                    onChange={(e) => {
+                      const img = e.target.files[0];
+                      setImagePreview(URL.createObjectURL(img));
+                      return setUser({ ...user, avatar: img });
+                    }}
                   />
                 </div>
               </div>
               <div className="pt-30">
-                <Input label="Full Name" />
+                <Input
+                  label="Full Name"
+                  value={user.name}
+                  onChange={(e: any) =>
+                    setUser({ ...user, name: e.target.value })
+                  }
+                />
               </div>
               <div className="pt-30">
-                <Input label="Email Address" />
+                <Input label="Email Address" disabled value={user.email} />
               </div>
-              <div className="pt-30">
+              {/* <div className="pt-30">
                 <Input label="Phone" />
-              </div>
+              </div> */}
               <div className="button-group d-flex flex-column pt-50">
                 <button
-                  type="submit"
+                  onClick={onSubmit}
+                  type="button"
                   className="btn btn-save fw-medium text-lg text-white rounded-pill"
-                  role="button"
                 >
                   Save My Profile
                 </button>

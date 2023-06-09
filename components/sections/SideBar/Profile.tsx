9eb5027c47d1e3b18af0990ab0f42dee98b92c2c
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-06-09 21:18:24 +0700

    refactor: image avatar radius

--- components/sections/SideBar/Profile.tsx
+++ components/sections/SideBar/Profile.tsx
@@ -27,7 +27,7 @@ export default function Profile() {
         width="90"
         height="90"
         className="img-fluid mb-20"
-        style={{ borderRadius: "100%" }}
+        style={{ borderRadius: "100%", objectFit: "cover" }}
       />
       <h2 className="fw-bold text-xl color-palette-1 m-0">{user.name}</h2>
       <p className="color-palette-2 m-0">{user.email}</p>

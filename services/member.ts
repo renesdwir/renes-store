cda20174c25b0c96b4af50d2401903d482555701
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-06-10 21:47:35 +0700

    refactor : params update profile func

--- services/member.ts
+++ services/member.ts
@@ -28,7 +28,7 @@ export async function getTransactionDetail(id: string, token: string) {
   return callAPI({ url, method: "GET", serverToken: token });
 }
 
-export async function updateProfile(data: FormData, id: string) {
-  const url = `${BASE_URL}/${API_VERSION}/players/profile/${id}`;
+export async function updateProfile(data: FormData) {
+  const url = `${BASE_URL}/${API_VERSION}/players/profile`;
   return callAPI({ url, method: "PUT", data, token: true });
 }
